d2b18c65012aea7c65ae714362fe67461bcd4e7e
Vladimir Levchuk <salyarka@gmail.com>
2017-06-03 21:33:39 +0300

    Added blank templates for discipline and students, and views function for thens.

--- app/views.py
+++ app/views.py
@@ -5,3 +5,13 @@ from app import app
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+@app.route('/discipline')
+def discipline():
+    return render_template('discipline.html')
+
+
+@app.route('/student')
+def student():
+    return render_template('student.html')
